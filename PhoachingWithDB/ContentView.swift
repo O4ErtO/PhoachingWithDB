dead94d24c0cddb5932397000a84c4649852091d
O4ErtO <vekhin200631@gmail.com>
2023-07-14 12:28:13 +0300

    Initial Commit

---
+++ PhoachingWithDB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhoachingWithDB
+//
+//  Created by Artem Vekshin on 14.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
